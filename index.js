058b6eb6ad6131882a85ae6de127689f6608da52
Ayokunnumi1 <omololuayk@gmail.com>
2023-07-20 03:49:00 +0100

    Add new classes to html elements & select them with javascript

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
-const gameOptions = ['rock', 'paper', 'scissors']
+const gameOptions = ['rock', 'paper', 'scissors'];
+const rockImage = document.querySelector('.rock-img');
+const paperImage = document.querySelector('.paper-img');
+const scissorsImage = document.querySelector('.scissors-img');
+const playerQuestionMark = document.querySelector('.player-question-mark');
+const computerQuestionMark = document.querySelector('.computer-question-mark');
+
+rockImage.addEventListener('click', () => {
+    playerQuestionMark.src = 'images/rock-icon.svg';
+});
+
+paperImage.addEventListener('click', () => {
+    playerQuestionMark.src = 'images/paper-icon.svg';
+});
+
+scissorsImage.addEventListener('click', () => {
+    playerQuestionMark.src = 'images/scissors-icon.svg';
+});
+
+
 function getComputerChoice() {
    const computerIndex = Math.floor(Math.random() * gameOptions.length);
-   const computerChoice = gameOptions[computerIndex];
+    const computerChoice = gameOptions[computerIndex];
+    if (computerChoice === 'rock') {
+       computerQuestionMark.src =  'images/rock-icon.svg';
+    }
+    else if (computerChoice === 'paper') {
+        computerQuestionMark.src =  'images/paper-icon.svg';
+    }
+    else if (computerChoice === 'scissors') {
+        computerQuestionMark.src =  'images/scissors-icon.svg';
+    }
+    
     return computerChoice;
 }
 
+
 function playRound(playerSelection, computerSelection) {
-    const lowerCasePlayerSelection = playerSelection.toLowerCase();
-    if (lowerCasePlayerSelection === computerSelection) {
+    if (playerSelection === computerSelection) {
         return "It's a tie";
     }
 
-    else if ((lowerCasePlayerSelection === 'rock' && computerSelection === 'scissors') ||
-        (lowerCasePlayerSelection === 'scissors' && computerSelection === 'paper') ||
-        (lowerCasePlayerSelection === 'paper' && computerSelection === 'rock'))
+    else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper' ) ||
+        (playerSelection === 'paper' && computerSelection === 'rock'))
     {
         return 'Player won, computer lose';
-    } else if ((computerSelection === 'rock' && lowerCasePlayerSelection === 'scissors') ||
-        (computerSelection === 'scissors' && lowerCasePlayerSelection === 'paper') ||
-        (computerSelection === 'paper' && lowerCasePlayerSelection === 'rock')) {
+    } else if ((computerSelection === 'rock' && playerSelection === 'scissors') ||
+        (computerSelection === 'scissors' && playerSelection === 'paper') ||
+        (computerSelection === 'paper' && playerSelection === 'rock')) {
         return 'Computer won, Player lose';
     }
 
